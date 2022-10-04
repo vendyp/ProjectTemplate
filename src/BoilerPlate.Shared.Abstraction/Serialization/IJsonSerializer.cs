8cece6bd9bc2d96efc55ce8bd739057691d9e8e4
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-04 10:40:49 +0700

    Update IJsonSerializer in shared abstraction

--- src/BoilerPlate.Shared.Abstraction/Serialization/IJsonSerializer.cs
+++ src/BoilerPlate.Shared.Abstraction/Serialization/IJsonSerializer.cs
@@ -3,6 +3,6 @@
 public interface IJsonSerializer
 {
     string Serialize<T>(T value);
-    T Deserialize<T>(string value);
-    object Deserialize(string value, Type type);
+    T? Deserialize<T>(string value);
+    object? Deserialize(string value, Type type);
 }
