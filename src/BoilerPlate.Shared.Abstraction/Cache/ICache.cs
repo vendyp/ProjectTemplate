f6140202d5eea1da269945bdfd0703e7d3a285ef
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-04 09:29:08 +0700

    Minor update in ICache GetAsync<T> return nullable T

--- src/BoilerPlate.Shared.Abstraction/Cache/ICache.cs
+++ src/BoilerPlate.Shared.Abstraction/Cache/ICache.cs
@@ -2,7 +2,7 @@
 
 public interface ICache
 {
-    Task<T> GetAsync<T>(string key);
+    Task<T?> GetAsync<T>(string key);
     Task<IReadOnlyList<T>> GetManyAsync<T>(params string[] keys);
     Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
     Task DeleteAsync<T>(string key);
