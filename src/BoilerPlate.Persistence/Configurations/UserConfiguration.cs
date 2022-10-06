f618c94ad85f78441f38d5225e0a07f0d4f9a2b7
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-06 17:25:22 +0700

    Update user configuration default add indexing on normalized username also fullname

--- src/BoilerPlate.Persistence/Configurations/UserConfiguration.cs
+++ src/BoilerPlate.Persistence/Configurations/UserConfiguration.cs
@@ -13,8 +13,10 @@ public sealed class UserConfiguration : BaseEntityConfiguration<User>
         builder.Property(e => e.UserId).ValueGeneratedNever();
         builder.Property(e => e.Username).HasMaxLength(256);
         builder.Property(e => e.NormalizedUsername).HasMaxLength(256);
+        builder.HasIndex(e => e.NormalizedUsername);
         builder.Property(e => e.Password).HasMaxLength(1024);
         builder.Property(e => e.FullName).HasMaxLength(256);
+        builder.HasIndex(e => e.FullName);
         builder.Property(e => e.PhoneNumber).HasMaxLength(256);
         builder.Property(e => e.Country).HasMaxLength(256);
         builder.Property(e => e.AboutMe).HasMaxLength(256);
