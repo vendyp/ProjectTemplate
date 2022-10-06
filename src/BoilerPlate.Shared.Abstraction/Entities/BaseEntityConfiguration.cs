87fb10ff194e8ddfd4710989df693c3da5fab77e
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-06 14:00:52 +0700

    Add BaseEntityConfiguration in abstraction

---
+++ src/BoilerPlate.Shared.Abstraction/Entities/BaseEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BoilerPlate.Shared.Abstraction.Entities;
+
+public abstract class BaseEntityConfiguration<TBaseEntity> : IEntityTypeConfiguration<TBaseEntity>
+    where TBaseEntity : BaseEntity
+{
+    public void Configure(EntityTypeBuilder<TBaseEntity> builder)
+    {
+        builder.Property(e => e.CreatedBy).HasMaxLength(maxLength: 256);
+        builder.Property(e => e.CreatedByName).HasMaxLength(maxLength: 256);
+
+        builder.Property(e => e.LastUpdatedBy).HasMaxLength(maxLength: 256);
+        builder.Property(e => e.LastUpdatedByName).HasMaxLength(maxLength: 256);
+
+        builder.Property(e => e.DeletedBy).HasMaxLength(maxLength: 256);
+        builder.Property(e => e.DeletedByName).HasMaxLength(maxLength: 256);
+
+        EntityConfiguration(builder);
+    }
+
+    protected abstract void EntityConfiguration(EntityTypeBuilder<TBaseEntity> builder);
+}
