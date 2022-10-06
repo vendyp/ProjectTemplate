54fc135a537efcd56e0c62bda2848242397c3266
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-06 14:15:59 +0700

    Update baseentity add createdat/lastupdatedat/deletedbyat server

--- src/BoilerPlate.Shared.Abstraction/Entities/BaseEntity.cs
+++ src/BoilerPlate.Shared.Abstraction/Entities/BaseEntity.cs
@@ -5,12 +5,15 @@ public abstract class BaseEntity
     public string? CreatedBy { get; set; }
     public string? CreatedByName { get; set; }
     public DateTime? CreatedAt { get; set; }
+    public DateTime? CreatedAtServer { get; set; }
 
     public string? LastUpdatedBy { get; set; }
     public string? LastUpdatedByName { get; set; }
     public DateTime? LastUpdatedAt { get; set; }
+    public DateTime? LastUpdatedAtServer { get; set; }
 
     public string? DeletedBy { get; set; }
     public string? DeletedByName { get; set; }
     public DateTime? DeletedByAt { get; set; }
+    public DateTime? DeletedByAtServer { get; set; }
 }
