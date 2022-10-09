bc3cb4ceca22465539c800a9a366f198246cc739
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-09 09:47:47 +0700

    Update documentation xml in permissionservice

--- src/BoilerPlate.Core/Abstractions/IPermissionService.cs
+++ src/BoilerPlate.Core/Abstractions/IPermissionService.cs
@@ -4,6 +4,19 @@ namespace BoilerPlate.Core.Abstractions;
 
 public interface IPermissionService
 {
+    /// <summary>
+    /// Get permission by id
+    /// </summary>
+    /// <param name="id">a string of value id</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete</param>
+    /// <returns></returns>
     Task<Permission?> GetPermissionByIdAsync(string id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Check all parameters of ids is valid
+    /// </summary>
+    /// <param name="ids">Make sure parameter that passed here doesnt have duplication</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete</param>
+    /// <returns></returns>
     Task<bool> AllIdIsValid(string[] ids, CancellationToken cancellationToken);
 }
