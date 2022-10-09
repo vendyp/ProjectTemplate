d0bc474926fa37aa7a3a7b4d3a71efc0e8bd475b
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-09 09:43:26 +0700

    Add abstraction of IPermissionService and its implementation

---
+++ src/BoilerPlate.Core/Abstractions/IPermissionService.cs
@@ -0,0 +1,9 @@
+﻿using BoilerPlate.Domain.Entities;
+
+namespace BoilerPlate.Core.Abstractions;
+
+public interface IPermissionService
+{
+    Task<Permission?> GetPermissionByIdAsync(string id, CancellationToken cancellationToken);
+    Task<bool> AllIdIsValid(string[] ids, CancellationToken cancellationToken);
+}
---
+++ src/BoilerPlate.Infrastructure/Services/PermissionService.cs
@@ -0,0 +1,33 @@
+﻿using BoilerPlate.Core.Abstractions;
+using BoilerPlate.Domain.Entities;
+using BoilerPlate.Shared.Abstraction.Databases;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoilerPlate.Infrastructure.Services;
+
+internal class PermissionService : IPermissionService
+{
+    private readonly IDbContext _dbContext;
+
+    public PermissionService(IDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<Permission?> GetPermissionByIdAsync(string id, CancellationToken cancellationToken)
+        => _dbContext.Set<Permission>().Where(e => e.Id == id).FirstOrDefaultAsync(cancellationToken);
+
+    public async Task<bool> AllIdIsValid(string[] ids, CancellationToken cancellationToken)
+    {
+        var listId = ids.Distinct().ToList();
+        if (listId.Count != ids.Length)
+            throw new InvalidOperationException("Result may have duplicate value");
+
+        var listPermission = await _dbContext.Set<Permission>().AsNoTracking().Where(e => listId.Contains(e.Id))
+            .ToListAsync(cancellationToken);
+
+        if (!listPermission.Any()) return false;
+
+        return listPermission.Count == listId.Count;
+    }
+}
