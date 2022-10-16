c91142f244c47a27882412fa688e01568e9c8b15
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-16 14:05:13 +0700

    Add extensions of creating claims

---
+++ src/BoilerPlate.Core/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace BoilerPlate.Core;
+
+public static class Extensions
+{
+    public static Dictionary<string, IEnumerable<string>> GenerateCustomClaims(User user,
+        DeviceType deviceType)
+    {
+        var claims = new Dictionary<string, IEnumerable<string>>
+        {
+            ["xid"] = new[] { user.UserId.ToString() },
+            ["givenName"] = new[] { user.Username },
+            ["usr"] = new[] { user.Username },
+            ["deviceType"] = new[] { deviceType.ToString() }
+        };
+
+        return claims;
+    }
+}
