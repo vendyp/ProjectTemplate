7e8a022d5ccd7dfc2338d8fda501d4e11f3f614c
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-16 14:27:50 +0700

    Add produces default content type attribute

---
+++ src/BoilerPlate.Shared.Infrastructure/Api/ProducesDefaultContentTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoilerPlate.Shared.Infrastructure.Api;
+
+public class ProducesDefaultContentTypeAttribute : ProducesAttribute
+{
+    public ProducesDefaultContentTypeAttribute(Type type) : base(type)
+    {
+    }
+
+    public ProducesDefaultContentTypeAttribute(params string[] additionalContentTypes)
+        : base("application/json", additionalContentTypes)
+    {
+    }
+}
