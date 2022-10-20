00188efa42f0b3702d50e92f41aa94c959f1a8fc
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-20 23:53:15 +0700

    Add new interface IEmailService

---
+++ src/BoilerPlate.Core/Abstractions/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace BoilerPlate.Core.Abstractions;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string from, string to, string subject, string htmlContent,
+        CancellationToken cancellationToken);
+}
