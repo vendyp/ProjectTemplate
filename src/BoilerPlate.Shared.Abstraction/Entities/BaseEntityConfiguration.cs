b10b00427576cc005407c173d878acf51ecdf0b2
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-21 22:11:16 +0700

    Update base entity add indexing on created/lastupdate/delete at

--- src/BoilerPlate.Shared.Abstraction/Entities/BaseEntityConfiguration.cs
+++ src/BoilerPlate.Shared.Abstraction/Entities/BaseEntityConfiguration.cs
@@ -10,12 +10,17 @@ public abstract class BaseEntityConfiguration<TBaseEntity> : IEntityTypeConfigur
     {
         builder.Property(e => e.CreatedBy).HasMaxLength(maxLength: 256);
         builder.Property(e => e.CreatedByName).HasMaxLength(maxLength: 256);
+        builder.HasIndex(e => e.CreatedAt);
+        builder.HasIndex(e => e.CreatedAtServer);
 
         builder.Property(e => e.LastUpdatedBy).HasMaxLength(maxLength: 256);
         builder.Property(e => e.LastUpdatedByName).HasMaxLength(maxLength: 256);
+        builder.HasIndex(e => e.LastUpdatedAt);
+        builder.HasIndex(e => e.LastUpdatedAtServer);
 
         builder.Property(e => e.DeletedBy).HasMaxLength(maxLength: 256);
         builder.Property(e => e.DeletedByName).HasMaxLength(maxLength: 256);
+        builder.HasIndex(e => e.DeletedByAt);
 
         builder.HasQueryFilter(e => e.DeletedByAt == null);
 
